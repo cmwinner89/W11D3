6e4de5953014bcb2c050af5597c2d8831abe00b2
Chris Joo <chrisj1225@gmail.com>
2021-05-05 14:44:24 -0400

    Created PokemonIndexItem Link component

--- poke_dex_pt_2/frontend/pokedex.jsx
+++ poke_dex_pt_2/frontend/pokedex.jsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
 
-
 document.addEventListener("DOMContentLoaded", () => {
+  console.log("test");
   const rootEl = document.getElementById("root");
   const store = configureStore();
   ReactDOM.render(<Root store={store} />, rootEl)
